a1f81f9db430e671a2177422807b69719c47970e
Max6711 <Maks1321321@gmail.com>
2021-10-30 22:21:17 +0300

    first_load

--- app/src/main/java/com/example/projectforbagrut/MainActivity.java
+++ app/src/main/java/com/example/projectforbagrut/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.projectforbagrut;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    public void maps (View view){
+
+        Intent intent=new Intent();
+        intent.setClass(this, Maps_Activity.class);
+
+        startActivity(intent);
+        finish();
+
     }
 }
